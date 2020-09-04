b2a362c08cf6f0c6058ccb83c934af656c498ce3
Paige Miles <paige@lucas-miles.com>
2020-09-04 03:30:37 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,11 @@
 def find_max_value(array)
   count = 0
-  array_copy = []
+  max_value = -1
   while count < array.length do
-    array_copy.push(array[count])
+    if max_value < array[count]
+      max_value = array[count]
+    end
     count += 1
   end
-  array_copy.sort!
-  array_copy.last
+  max_value
 end
